4971f74a715c3c2121762ee775b4f9aa33d6e282
Astra <117833599+utkarshhh1@users.noreply.github.com>
2023-04-15 21:56:31 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,13 @@
+// async function signup(e)
+// {   e.preventDefault()
+//     const email = document.querySelector('#signupEmail')
+//     const password = document.querySelector('#signupPassword')
+//     try{
+//         const result = await firebase.auth().createUserWithEmailAndPassword(email.value, password.value)
+//         console.log(result)
+//     }catch(err)
+//     {
+//         console.log(err)
+//     }
+   
+// }
